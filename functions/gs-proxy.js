83e6b86e734179893e2ee1818f65cf0ee722c67f
tomtruong-sa <truongbathanh@gmail.com>
2025-10-21 09:29:40 -0600

    Create gs-proxy.js

---
+++ functions/gs-proxy.js
@@ -0,0 +1,45 @@
+export async function onRequestPost(context) {
+  try {
+    const { request } = context;
+    const body = await request.formData();
+
+    const target =
+      "https://script.google.com/macros/s/AKfycbyqQ8Dvb6M9FaULKa2_UGNaA-UTIRC2fhAteF4I5CYGAOIdRnhMkH54S39bbVj1quEz/exec";
+
+    const res = await fetch(target, {
+      method: "POST",
+      body,
+    });
+
+    const text = await res.text();
+
+    return new Response(text, {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ status: "error", message: err.message }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+  }
+}
+
+export async function onRequestOptions() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
+  });
+}
